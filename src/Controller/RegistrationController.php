3fb8b0ecf455110e7fda8327958f2ad4c406f3c9
Valdior <valdior.tenshi@gmail.com>
2022-12-16 10:58:21 +0100

    #3 phpcs & phpcbf

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -45,7 +45,9 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
                 (new TemplatedEmail())
                     ->from(new Address('welcome@imanarcher.be', 'Welcome | I\'m An Archer'))
                     ->to($user->getEmail())
