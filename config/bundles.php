8c9624a5d1ed6f7cd20063eda463e0deb0210dcf
Maréchal Pierre <valdior.tenshi@gmail.com>
2022-12-16 11:01:31 +0100

    #3 feat: register (#7)
    * #3 feat: register
    
    * #3 phpcs & phpcbf
    
    * #3 phpcbf & phpcs

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
