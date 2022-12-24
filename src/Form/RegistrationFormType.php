c21aeb6680a078e0b187e4be9603a62540fc9ebc
Valdior <valdior.tenshi@gmail.com>
2022-12-24 11:02:59 +0100

    #12 feat: add bootstrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,8 +20,19 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
+            ->add('username', TextType::class, [
+                'label' => 'form.user.username',
+                'attr' => ['placeholder' => 'form.user.username',
+                    'title' => 'form.user.username',
+                    ],
+                ])
+            ->add('email', EmailType::class, [
+                'label' => 'form.user.email',
+                'attr' => [
+                    'placeholder' => 'form.user.email',
+                    'title' => 'form.user.email',
+                    ],
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -27,11 +41,30 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'type' => PasswordType::class,
+                'required' => true,
+                'options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                    ],
+                ],
+                'first_options' => [
+                    'label' => 'form.user.password',
+                    'attr' => [
+                        'placeholder' => 'form.user.password',
+                        'title' => 'form.user.password',
+                        ],
+                ],
+                'second_options' => [
+                    'label' => 'form.user.password_confirmation',
+                    'attr' => [
+                        'placeholder' => 'form.user.password_confirmation',
+                        'title' => 'form.user.password_confirmation',
+                        ],
+                ],
+                'invalid_message' => 'form.user.mismatch',
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -51,6 +84,9 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'csrf_token_id' => 'registration',
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
         ]);
     }
 }
