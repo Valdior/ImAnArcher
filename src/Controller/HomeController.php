457603e0bd49f696d133c112eb3f552d6aaa66bf
Maréchal Pierre <valdior.tenshi@gmail.com>
2022-12-24 11:05:57 +0100

    #12 feat: add bootstrap (#13)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'current_menu' => 'home'
         ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -64,6 +64,7 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+            'current_menu' => 'register'
         ]);
     }
 
