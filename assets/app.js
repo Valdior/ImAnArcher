b519360c5d34518458bf0ce1d93ccf442203c652
Valdior <valdior.tenshi@gmail.com>
2022-12-24 12:13:32 +0100

    #5 feat: add login

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import { Dropdown } from 'bootstrap';
+
 // start the Stimulus application
 import './bootstrap';
