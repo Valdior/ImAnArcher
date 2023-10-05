1886d6c08763f24d2697db543e92266a162f1a32
Pierre Maréchal <pierre.marechal@nsi-sa.be>
2023-10-05 16:22:13 +0200

    #8 feat: * add translation * add style form fieldset * add style form tournament * update dependencies js

--- src/Form/PlatoonType.php
+++ src/Form/PlatoonType.php
@@ -9,18 +9,27 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class PlatoonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('maxParticipants')
+            ->add('maxParticipants', NumberType::class, [
+                'label' => 'form.platoon.maxParticipants',
+                'attr' => [
+                    'placeholder' => 'form.platoon.maxParticipants',
+                    'title' => 'form.platoon.maxParticipants',
+                    ],
+                ])
             ->add('type', EnumType::class, array(
+                'label' => 'form.platoon.type',
                 'class' => PlatoonTypeEnum::class,
-                'choice_label' => fn ($choice) => $choice->label()
+                'choice_label' => fn ($choice) => $choice->label(),
             ))
             ->add('startTime', DateTimeType::class, array(
+                'label' => 'form.platoon.startTime',
                 'widget' => 'single_text',
             ))
         ;
